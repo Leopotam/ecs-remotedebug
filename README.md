7da9bbf35e584d534c1d219a45b0d333396227a1
Leopotam <leopotam@gmail.com>
2020-03-15 02:49:29 +0300

    * RemoteDebugClient - test implementation.

---
+++ README.md
@@ -0,0 +1,59 @@
+[![gitter](https://img.shields.io/gitter/room/leopotam/ecs.svg)](https://gitter.im/leopotam/ecs)
+[![license](https://img.shields.io/github/license/Leopotam/ecs-remotedebug.svg)](https://github.com/Leopotam/ecs-remotedebug/blob/develop/LICENSE)
+# Remote debug connector for Entity Component System framework
+[Remote debug](https://github.com/Leopotam/ecs-remotedebug) for [ECS framework](https://github.com/Leopotam/ecs).
+
+> **Important! It's PoC, don't use it if you dont know what is it!**
+
+> C#7.3 or above required for this framework.
+
+> Tested on unity 2019.1 (not dependent on it) and contains assembly definition for compiling to separate assembly file for performance reason.
+
+> Dependent on [ECS framework](https://github.com/Leopotam/ecs) - ECS framework should be imported to unity project first.
+
+# Installation
+
+## As unity module
+This repository can be installed as unity module directly from git url. In this way new line should be added to `Packages/manifest.json`:
+```
+"com.leopotam.ecs-remotedebug": "https://github.com/Leopotam/ecs-remotedebug.git",
+```
+By default last released version will be used. If you need trunk / developing version then `develop` name of branch should be added after hash:
+```
+"com.leopotam.ecs-unityintegration": "https://github.com/Leopotam/ecs-remotedebug.git#develop",
+```
+
+## As source
+If you can't / don't want to use unity modules, code can be downloaded as sources archive of required release from [Releases page](`https://github.com/Leopotam/ecs-remotedebug/releases`).
+
+# Integration
+
+Integration can be processed with creation of `LeopotamGroup.Ecs.RemoteDebug` class instance - this call should be wrapped to `#if DEBUG` preprocessor define:
+```csharp
+public class Startup : MonoBehaviour {
+    EcsWorld _world;
+    EcsSystems _systems;
+
+    void Start () {
+        _world = new EcsWorld ();
+        _systems = new EcsSystems(_world);
+#if DEBUG
+        new Leopotam.Ecs.RemoteDebug.RemoteDebugClient (_world);
+#endif  
+        _systems.
+            .Add (new RunSystem1());
+            // Additional initialization here...
+            .Init ();
+    }
+}
+```
+
+`RemoteDebugClient` instance **must** be created before any entity will be created in ecs-world.
+
+# License
+The software released under the terms of the [MIT license](./LICENSE.md). Enjoy.
+
+# Donate
+Its free opensource software, but you can buy me a coffee:
+
+<a href="https://www.buymeacoffee.com/leopotam" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/yellow_img.png" alt="Buy Me A Coffee" style="height: auto !important;width: auto !important;" ></a>
---
+++ src/RemoteDebugClient.cs
@@ -0,0 +1,171 @@
+﻿// ----------------------------------------------------------------------------
+// The MIT License
+// Remote debug https://github.com/Leopotam/ecs-remotedebug
+// for ECS framework https://github.com/Leopotam/ecs
+// Copyright (c) 2017-2020 Leopotam <leopotam@gmail.com>
+// ----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Leopotam.Ecs.RemoteDebug {
+    public sealed class RemoteDebugClient : IEcsWorldDebugListener, IEcsSystemsDebugListener {
+        readonly EcsWorld _world;
+        readonly Uri _url;
+        readonly ClientWebSocket _ws = new ClientWebSocket ();
+        readonly JsonSerialization _json = new JsonSerialization ();
+        readonly object _syncObj = new object ();
+        readonly Queue<RemoteCmd> _outCmds = new Queue<RemoteCmd> (512);
+        readonly Queue<RemoteCmd> _inCmds = new Queue<RemoteCmd> (512);
+        object[] _componentValuesCache = new object[16];
+        readonly StringBuilder _sb = new StringBuilder (512);
+
+        public bool IsConnected => _ws.State == WebSocketState.Open;
+
+        public RemoteDebugClient (EcsWorld world, int port = 1111, string host = "localhost") {
+            _world = world;
+            _world.AddDebugListener (this);
+            _url = new Uri ($"ws://{host}:{port}");
+            Connect ();
+        }
+
+        async void Connect () {
+            try {
+                await _ws.ConnectAsync (_url, CancellationToken.None);
+                await Task.WhenAll (Receive (_ws), Send (_ws));
+            } catch {
+                // ignored
+            } finally {
+                if (IsConnected) {
+                    await _ws.CloseAsync (WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                }
+                UnityEngine.Debug.LogWarning ("STOPPED");
+            }
+        }
+
+        async Task Send (ClientWebSocket ws) {
+            while (IsConnected) {
+                string cmd = null;
+                lock (_syncObj) {
+                    if (_outCmds.Count > 0) {
+                        cmd = _json.Serialize (_outCmds.Dequeue ());
+                    }
+                }
+                if (cmd != null) {
+                    var buffer = Encoding.UTF8.GetBytes (cmd);
+                    await ws.SendAsync (new ArraySegment<byte> (buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                } else {
+                    await Task.Delay (1);
+                }
+            }
+        }
+
+        async Task Receive (ClientWebSocket webSocket) {
+            var buffer = new byte[4096];
+            while (IsConnected) {
+                var result = await webSocket.ReceiveAsync (new ArraySegment<byte> (buffer), CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close) {
+                    await webSocket.CloseAsync (WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                    return;
+                }
+                if (!result.EndOfMessage) {
+                    throw new Exception ("Partial messages not supported");
+                }
+                var cmd = _json.Deserialize<RemoteCmd> (Encoding.UTF8.GetString (buffer, 0, result.Count));
+                lock (_syncObj) {
+                    // FIXME: send to _inCmds queue and process in main thread of world?
+                    ProcessCmdRequest (cmd);
+                }
+            }
+        }
+
+        void ProcessCmdRequest (RemoteCmd cmd) {
+            switch (cmd.Cmd) {
+                case RemoteCmdType.RequestEntityData:
+                    if (cmd.Int1.HasValue && cmd.Int2.HasValue) {
+                        var entity = _world.RestoreEntityFromInternalId (cmd.Int1.Value, cmd.Int2.Value);
+                        if (entity.IsAlive ()) {
+                            var count = entity.GetComponentValues (ref _componentValuesCache);
+                            for (var i = 0; i < count; i++) {
+                                _sb.AppendLine (_json.Serialize (_componentValuesCache[i]));
+                            }
+                            var res = new RemoteCmd {
+                                Cmd = RemoteCmdType.RequestEntityData,
+                                Int1 = cmd.Int1,
+                                Int2 = cmd.Int2,
+                                Str1 = _sb.ToString ()
+                            };
+                            _sb.Length = 0;
+                            _outCmds.Enqueue (res);
+                        }
+                    }
+                    break;
+            }
+        }
+
+        void IEcsWorldDebugListener.OnEntityCreated (EcsEntity entity) {
+            var cmd = new RemoteCmd { Cmd = RemoteCmdType.EntityCreated, Int1 = entity.GetInternalId () };
+            lock (_syncObj) {
+                _outCmds.Enqueue (cmd);
+            }
+        }
+
+        void IEcsWorldDebugListener.OnEntityDestroyed (EcsEntity entity) {
+            var cmd = new RemoteCmd { Cmd = RemoteCmdType.EntityRemoved, Int1 = entity.GetInternalId () };
+            lock (_syncObj) {
+                _outCmds.Enqueue (cmd);
+            }
+        }
+
+        void IEcsWorldDebugListener.OnFilterCreated (EcsFilter filter) {
+            // FIXME: OnFilterCreated implementation.
+        }
+
+        void IEcsWorldDebugListener.OnComponentListChanged (EcsEntity entity) {
+            var cmd = new RemoteCmd { Cmd = RemoteCmdType.EntityChanged, Int1 = entity.GetInternalId () };
+            lock (_syncObj) {
+                _outCmds.Enqueue (cmd);
+            }
+        }
+
+        void IEcsWorldDebugListener.OnWorldDestroyed (EcsWorld world) {
+            if (world == _world) {
+                _world.RemoveDebugListener (this);
+                lock (_syncObj) {
+                    _outCmds.Clear ();
+                    _inCmds.Clear ();
+                    if (IsConnected) {
+                        _ws.CloseAsync (WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    }
+                }
+            }
+        }
+
+        void IEcsSystemsDebugListener.OnSystemsDestroyed (EcsSystems systems) {
+            // FIXME: OnFilterCreated implementation.
+        }
+
+        enum RemoteCmdType {
+            EntityCreated,
+            EntityRemoved,
+            EntityChanged,
+            RequestEntityData
+        }
+
+        struct RemoteCmd {
+            [JsonName ("cmd")]
+            public RemoteCmdType Cmd;
+            [JsonName ("int1")]
+            public int? Int1;
+            [JsonName ("int2")]
+            public int? Int2;
+            [JsonName ("str1")]
+            // ReSharper disable once NotAccessedField.Local
+            public string Str1;
+        }
+    }
+}
